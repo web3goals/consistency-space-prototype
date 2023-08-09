5008d959b5fdc4aa6153aa4a8f16480b0d0b8e06
kiv1n <aloha@kiv1n.ru>
2023-08-09 12:12:35 +0300

    Config app project, add landing page

---
+++ app/pages/activities/create.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/layout";
+import { useRouter } from "next/router";
+
+/**
+ * Page to create an activity.
+ */
+export default function ActivityCreate() {
+  const router = useRouter();
+
+  return <Layout maxWidth="sm">...</Layout>;
+}
