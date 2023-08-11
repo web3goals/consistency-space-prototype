b900920b23006e9c1ff983203e37b7708898e342
kiv1n <aloha@kiv1n.ru>
2023-08-11 10:35:06 +0300

    Add list with activities and button to check in to account page, update activity contract in the app

--- app/components/account/AccountActivities.tsx
+++ app/components/account/AccountActivities.tsx
@@ -1,9 +1,21 @@
+import { activityContractAbi } from "@/contracts/abi/acitivtyContract";
+import useToasts from "@/hooks/useToast";
+import { chainToSupportedChainConfig } from "@/utils/chains";
 import { Box, Stack, SxProps, Typography } from "@mui/material";
 import { orange } from "@mui/material/colors";
-import EntityList from "../entity/EntityList";
-import { CardBox, MediumLoadingButton } from "../styled";
+import { useEffect, useState } from "react";
 import CalendarHeatmap from "react-calendar-heatmap";
 import "react-calendar-heatmap/dist/styles.css";
+import {
+  useAccount,
+  useContractRead,
+  useContractWrite,
+  useNetwork,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
+import EntityList from "../entity/EntityList";
+import { CardBox, FullWidthSkeleton, MediumLoadingButton } from "../styled";
 
 /**
  * A component with account activities.
@@ -12,67 +24,253 @@ export default function AccountActivities(props: {
   address: `0x${string}`;
   sx?: SxProps;
 }) {
+  const { chain } = useNetwork();
+
   /**
-   * TODO:
-   *
-   * 1. Load ids of activities from contract using moralis (https://docs.moralis.io/web3-data-api/evm/reference/get-wallet-nfts)
-   * 3. Load activity params, check-ins, heatmap by id
-   * 2. Calculate heatmap by activity check-ins
+   * Define account balance of activities
    */
+  const { data: balance } = useContractRead({
+    address: chainToSupportedChainConfig(chain).contracts.activity,
+    abi: activityContractAbi,
+    functionName: "balanceOf",
+    args: [props.address],
+  });
+  const tokenIndexes =
+    balance !== undefined
+      ? [...Array.from(Array(Number(balance)).keys())].reverse()
+      : undefined;
 
   return (
     <>
       <EntityList
-        entities={[]}
-        renderEntityCard={({}, index) => <AccountActivityCard key={index} />}
+        entities={tokenIndexes}
+        renderEntityCard={(tokenIndex, index) => (
+          <AccountActivityCard
+            address={props.address}
+            tokenIndex={tokenIndex}
+            key={index}
+          />
+        )}
         noEntitiesText="😐 no activities"
         sx={{ mt: 4 }}
       />
-      <AccountActivityCardFake />
     </>
   );
 }
 
-function AccountActivityCard(props: {}) {
-  return <CardBox>...</CardBox>;
+function AccountActivityCard(props: {
+  address: `0x${string}`;
+  tokenIndex: number;
+}) {
+  const { chain } = useNetwork();
+
+  /**
+   * Define token id
+   */
+  const { data: tokenId } = useContractRead({
+    address: chainToSupportedChainConfig(chain).contracts.activity,
+    abi: activityContractAbi,
+    functionName: "tokenOfOwnerByIndex",
+    args: [props.address, BigInt(props.tokenIndex)],
+  });
+
+  /**
+   * Define token params
+   */
+  const { data: tokenParams } = useContractRead({
+    address: chainToSupportedChainConfig(chain).contracts.activity,
+    abi: activityContractAbi,
+    functionName: "getParams",
+    args: [tokenId as bigint],
+    enabled: tokenId !== undefined,
+  });
+
+  /**
+   * Define token check-ins
+   */
+  const { data: tokenCheckIns, refetch: refetchTokenCheckIns } =
+    useContractRead({
+      address: chainToSupportedChainConfig(chain).contracts.activity,
+      abi: activityContractAbi,
+      functionName: "getCheckIns",
+      args: [tokenId as bigint],
+      enabled: tokenId !== undefined,
+    });
+
+  if (tokenId && tokenParams && tokenCheckIns) {
+    return (
+      <CardBox>
+        <Typography variant="h6" fontWeight={700}>
+          {tokenParams.description}
+        </Typography>
+        <Stack direction="row" spacing={1} mt={1}>
+          <Typography color={orange[500]} fontWeight={700}>
+            {tokenCheckIns.length}
+          </Typography>
+          <Typography>check-ins total</Typography>
+        </Stack>
+        {tokenParams.checkInRequirement === 1 && (
+          <Stack direction="row" spacing={1} mt={0.5}>
+            <Typography color={orange[500]} fontWeight={700}>
+              Everyone
+            </Typography>
+            <Typography>can check in this activity</Typography>
+          </Stack>
+        )}
+        <AccountActivityCardActions
+          address={props.address}
+          tokenId={tokenId}
+          tokenCheckInRequirement={tokenParams.checkInRequirement}
+          onCheckedIn={refetchTokenCheckIns}
+          sx={{ mt: 1 }}
+        />
+        <AccountActivityCardCalendar
+          tokenCheckIns={tokenCheckIns}
+          sx={{ mt: 4, mb: 4 }}
+        />
+        <AccountActivityCardReactions />
+      </CardBox>
+    );
+  }
+
+  return <FullWidthSkeleton />;
 }
 
-function AccountActivityCardFake() {
+function AccountActivityCardActions(props: {
+  address: `0x${string}`;
+  tokenId: bigint;
+  tokenCheckInRequirement: number;
+  onCheckedIn: () => void;
+  sx?: SxProps;
+}) {
+  const { address } = useAccount();
+
   return (
-    <CardBox sx={{ mt: 2 }}>
-      <Typography variant="h6" fontWeight={700}>
-        🙌 Helping beginner developers
-      </Typography>
-      <Stack direction="row" spacing={1} mt={1}>
-        <Typography color={orange[500]} fontWeight={700}>
-          42
-        </Typography>
-        <Typography>check-ins this month</Typography>
-      </Stack>
-      <Stack direction="row" spacing={1} mt={0.5}>
-        <Typography color={orange[500]} fontWeight={700}>
-          Everyone
-        </Typography>
-        <Typography>can check in this activity</Typography>
-      </Stack>
-      <Stack direction="row" spacing={2} mt={1}>
-        <MediumLoadingButton variant="contained">
-          ✅ Check In
-        </MediumLoadingButton>
-        <MediumLoadingButton variant="outlined">📢 Share</MediumLoadingButton>
-      </Stack>
-      <Box sx={{ my: 4 }}>
-        <CalendarHeatmap
-          startDate={new Date("2023-06-01")}
-          endDate={new Date("2023-08-31")}
-          values={[
-            { date: "2023-08-08", count: 12 },
-            { date: "2023-08-09", count: 38 },
-          ]}
-          titleForValue={(value) => `Date is ${JSON.stringify(value)}`}
+    <Stack direction="row" spacing={2} sx={{ ...props.sx }}>
+      {props.tokenCheckInRequirement === 0 && props.address === address && (
+        <AccountActivityCardActionsCheckInButton
+          tokenId={props.tokenId}
+          onCheckedIn={props.onCheckedIn}
         />
-      </Box>
-      <Stack direction="row" spacing={1} mt={1} alignItems="center">
+      )}
+      {props.tokenCheckInRequirement === 1 && (
+        <AccountActivityCardActionsCheckInButton
+          tokenId={props.tokenId}
+          onCheckedIn={props.onCheckedIn}
+        />
+      )}
+      {/* TODO: Implement button */}
+      <MediumLoadingButton variant="outlined">📢 Share</MediumLoadingButton>
+    </Stack>
+  );
+}
+
+function AccountActivityCardActionsCheckInButton(props: {
+  tokenId: bigint;
+  onCheckedIn: () => void;
+}) {
+  const { chain } = useNetwork();
+  const { showToastSuccess } = useToasts();
+
+  /**
+   * Contract states
+   */
+  const { config: contractConfig } = usePrepareContractWrite({
+    address: chainToSupportedChainConfig(chain).contracts.activity,
+    abi: activityContractAbi,
+    functionName: "checkIn",
+    args: [props.tokenId],
+    chainId: chainToSupportedChainConfig(chain).chain.id,
+  });
+  const {
+    data: contractWriteData,
+    isLoading: isContractWriteLoading,
+    write: contractWrite,
+  } = useContractWrite(contractConfig);
+  const { isLoading: isTransactionLoading, isSuccess: isTransactionSuccess } =
+    useWaitForTransaction({
+      hash: contractWriteData?.hash,
+    });
+
+  /**
+   * Form states
+   */
+  const isButtonLoading = isContractWriteLoading || isTransactionLoading;
+  const isButtonDisabled =
+    isButtonLoading || isTransactionSuccess || !contractWrite;
+
+  useEffect(() => {
+    if (isTransactionSuccess) {
+      showToastSuccess("Activity is checked in");
+      props.onCheckedIn();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isTransactionSuccess]);
+
+  return (
+    <MediumLoadingButton
+      variant="contained"
+      loading={isButtonLoading}
+      disabled={isButtonDisabled}
+      onClick={() => contractWrite?.()}
+    >
+      ✅ Check In
+    </MediumLoadingButton>
+  );
+}
+
+function AccountActivityCardCalendar(props: {
+  tokenCheckIns: readonly bigint[];
+  sx?: SxProps;
+}) {
+  const [config, setConfig] = useState<
+    | {
+        startDate: Date;
+        endDate: Date;
+        values: { date: string; count: number }[];
+      }
+    | undefined
+  >();
+
+  useEffect(() => {
+    // Build map with dates and counts
+    const dateCounts = new Map();
+    for (const checkIn of props.tokenCheckIns) {
+      const date = new Date(Number(checkIn) * 1000).toISOString().split("T")[0];
+      dateCounts.set(date, (dateCounts.get(date) || 0) + 1);
+    }
+    // Set config
+    setConfig({
+      startDate: new Date("2023-03-15"),
+      endDate: new Date(),
+      values: Array.from(dateCounts).map((value) => ({
+        date: value[0],
+        count: value[1],
+      })),
+    });
+  }, [props.tokenCheckIns]);
+
+  return (
+    <Box sx={{ ...props.sx }}>
+      {config ? (
+        <CalendarHeatmap
+          startDate={config.startDate}
+          endDate={config.endDate}
+          values={config.values}
+          titleForValue={(value) => `${value?.count || 0} check-ins`}
+        />
+      ) : (
+        <FullWidthSkeleton />
+      )}
+    </Box>
+  );
+}
+
+// TODO: Display real reactions
+function AccountActivityCardReactions() {
+  return (
+    <Stack spacing={1}>
+      <Stack direction="row" spacing={1} alignItems="center">
         <MediumLoadingButton
           variant="contained"
           sx={{
@@ -88,7 +286,7 @@ function AccountActivityCardFake() {
           7
         </Typography>
       </Stack>
-      <Stack direction="row" spacing={1} mt={1} alignItems="center">
+      <Stack direction="row" spacing={1} alignItems="center">
         <MediumLoadingButton
           variant="outlined"
           sx={{
@@ -106,6 +304,6 @@ function AccountActivityCardFake() {
           19
         </Typography>
       </Stack>
-    </CardBox>
+    </Stack>
   );
 }
--- app/contracts/abi/acitivtyContract.ts
+++ app/contracts/abi/acitivtyContract.ts
@@ -481,6 +481,49 @@ export const activityContractAbi = [
     inputs: [
       {
         internalType: "uint256",
+        name: "index",
+        type: "uint256",
+      },
+    ],
+    name: "tokenByIndex",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "index",
+        type: "uint256",
+      },
+    ],
+    name: "tokenOfOwnerByIndex",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
         name: "tokenId",
         type: "uint256",
       },
@@ -491,6 +534,19 @@ export const activityContractAbi = [
         internalType: "string",
         name: "",
         type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
       },
     ],
     stateMutability: "view",
